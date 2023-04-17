b5d6984538d14965e06b1024f47679aa92905e33
basilio007 <113118989+basilio007@users.noreply.github.com>
2023-04-17 17:17:03 +0200

    drd

--- Servidor_Cliente/src/clases/ServidorMain.java
+++ Servidor_Cliente/src/clases/ServidorMain.java
@@ -1,13 +1,18 @@
 package clases;
 
 import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class ServidorMain {
 
 	public static void main(String[] args) throws IOException {
-		Servidor servidor = new Servidor();
-		System.out.println("Servidor iniciado");
-		servidor.runServer();
+		ServerSocket servidor = null;
+		Socket sock =  null;
+		int puerto = 1234;
+		
+		servidor = new ServerSocket(puerto);
+	
 	}
 
 }
--- proyectoclienteservidor/src/clases/ClienteMain.java
+++ proyectoclienteservidor/src/clases/ClienteMain.java
@@ -5,9 +5,7 @@ import java.net.UnknownHostException;
 public class ClienteMain {
 
 	public static void main(String[] args) throws UnknownHostException, IOException {
-		Cliente cliente = new Cliente();
-		cliente.runCliente();
-
+		
 	}
 
 }
