7605fcdbdee5537db21f5a6efa5c22fda0b47f58
basilio007 <113118989+basilio007@users.noreply.github.com>
2023-04-17 23:38:52 +0200

    both classes

--- Servidor_Cliente/src/clases/ServidorMain.java
+++ Servidor_Cliente/src/clases/ServidorMain.java
@@ -1,18 +1,45 @@
 package clases;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class ServidorMain {
 
-	public static void main(String[] args) throws IOException {
-		ServerSocket servidor = null;
-		Socket sock =  null;
-		int puerto = 1234;
-		
-		servidor = new ServerSocket(puerto);
+	public static void main(String[] args) {
+	ServerSocket servidor = null;
+	Socket sock = null;
+	int PUERTO = 1234;
+	DataInputStream in;
+	DataOutputStream out;
 	
+	try {
+		servidor = new ServerSocket();
+		System.out.println("Servidor inicializado...OK");
+		
+		
+		while(true) {
+			sock = servidor.accept();
+			System.out.println("cliente conectado...OK");
+			in = new DataInputStream(sock.getInputStream());
+			out = new DataOutputStream(sock.getOutputStream());
+			
+			String mensaje = in.readUTF();
+			
+			System.out.println(mensaje);
+			out.writeUTF("hola humano soy el servidor del sistema");
+			
+			sock.close();
+			System.out.println("cliente desconectado...OK");
+			
+		}
+		
+		
+	} catch (IOException e) {
+		
+		e.printStackTrace();
 	}
-
+	}
 }
--- proyectoclienteservidor/src/clases/ClienteMain.java
+++ proyectoclienteservidor/src/clases/ClienteMain.java
@@ -1,11 +1,41 @@
 package clases;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.net.Socket;
 import java.net.UnknownHostException;
 
 public class ClienteMain {
 
-	public static void main(String[] args) throws UnknownHostException, IOException {
+	public static void main(String[] args){
+	
+		final String HOST = "localhost";
+		final int PUERTO = 1234;
+		DataInputStream in;
+		DataOutputStream out;
 		
+		try {
+			Socket  soc = new Socket(HOST, PUERTO );
+			
+			in = new DataInputStream(soc.getInputStream());
+			out = new DataOutputStream(soc.getOutputStream());
+			
+			out.writeUTF("fkdjfjfkfdk");
+			
+			String mensaje = in.readUTF();
+			
+			System.out.println(mensaje);
+			
+			soc.close();
+			
+			
+			
+			
+			
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+		}
 	}
 
 }
