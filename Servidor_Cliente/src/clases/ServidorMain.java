cc36a4b7f3ef745a1f73136b485d90838a607295
basilio007 <113118989+basilio007@users.noreply.github.com>
2023-04-19 20:06:07 +0200

    done

--- Servidor_Cliente/src/clases/ServidorMain.java
+++ Servidor_Cliente/src/clases/ServidorMain.java
@@ -1,48 +1,101 @@
 package clases;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.io.*;
+import java.net.*;
 
 public class ServidorMain {
+    public static void main(String args[]) {
+    int PUERTO = 1234;
+    ServidorMain server = new ServidorMain(PUERTO );
+    server.startServer();
+    }
 
-	public static void main(String[] args) {
-		
-	ServerSocket servidor = null;
-	Socket sock = null;
-	 final int PUERTO = 1234;
-	
-	DataInputStream in;
-	DataOutputStream out;
-	
-	try {
-		servidor = new ServerSocket(PUERTO);
-		System.out.println("Servidor inicializado...OK");
-		
-		
-		while(true) {
-			sock = servidor.accept();
-			System.out.println("cliente conectado...OK");
-			in = new DataInputStream(sock.getInputStream());
-			out = new DataOutputStream(sock.getOutputStream());
-			
-			String mensaje = in.readUTF();
-			
-			System.out.println(mensaje);
-			out.writeUTF("hola humano soy el servidor del sistema");
-			
-			sock.close();
-			System.out.println("cliente desconectado...OK");
-			System.out.print("");
-			
-		}
-		
-		
-	} catch (IOException e) {
-		
-		e.printStackTrace();
-	}
-	}
+    // declarando un socket de servidor
+
+    ServerSocket echoServer = null;
+    Socket clientSocket = null;
+    int port;
+
+    public ServidorMain( int port ) {
+    this.port = port;
+    }
+
+    public void stopServer() {
+    System.out.println( "limpieza del servidor...OK." );
+    System.exit(0);
+    }
+
+    public void startServer() {
+    // para abrir el socket del servidor en el puerto dado   
+
+        try {
+        echoServer = new ServerSocket(port);
+        }
+        catch (IOException e) {
+        System.out.println(e);
+        }   
+
+    System.out.println( "Esperando conección...OK" );
+
+    // creando un objeto socket desde el ServerSocket para escucha y aceptar la coneccion.
+    // Usando ConnecionServidor para procesar la coneccion.
+
+    while ( true ) {
+        try {
+        clientSocket = echoServer.accept();
+        ConnecionServidor oneconnection = new ConnecionServidor(clientSocket, this);
+        oneconnection.run();
+        }   
+        catch (IOException e) {
+        System.out.println(e);
+        }
+    }
+    }
+}
+
+class ConnecionServidor {
+    BufferedReader is;
+    PrintStream os;
+    Socket clientSocket;
+    ServidorMain server;
+
+    public ConnecionServidor(Socket clientSocket, ServidorMain server) {
+    this.clientSocket = clientSocket;
+    this.server = server;
+    System.out.println( "Conección establecida...OK: ");
+    try {
+        is = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        os = new PrintStream(clientSocket.getOutputStream());
+    } catch (IOException e) {
+        System.out.println(e);
+    }
+    }
+
+    public void run() {
+        String line;
+    try {
+        boolean serverStop = false;
+
+            while (true) {
+                line = is.readLine();
+        System.out.println( "el mensaje recibido es: " + line );
+        String n = (line);
+        if ( n.equals("salir")) {
+            serverStop = true;
+            break;
+        }
+        if ( n.equals("parar")) break;
+                os.println("" + n ); 
+            }
+
+        System.out.println( "Conección terminada...OK...." );
+            is.close();
+            os.close();
+            clientSocket.close();
+
+        if ( serverStop ) server.stopServer();
+    } catch (IOException e) {
+        System.out.println(e);
+    }
+    }
 }
--- proyectoclienteservidor/src/clases/ClienteMain.java
+++ proyectoclienteservidor/src/clases/ClienteMain.java
@@ -1,41 +1,74 @@
 package clases;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.net.Socket;
-import java.net.UnknownHostException;
+import java.io.*;
+import java.net.*;
 
 public class ClienteMain {
+    public static void main(String[] args) {
 
-	public static void main(String[] args){
-	
-		final String HOST = "localhost";
-		final int PUERTO = 1234;
-		DataInputStream in;
-		DataOutputStream out;
-		
-		try {
-			Socket  soc = new Socket(HOST, PUERTO );
-			
-			in = new DataInputStream(soc.getInputStream());
-			out = new DataOutputStream(soc.getOutputStream());
-			
-			out.writeUTF("fkdjfjfkfdk");
-			
-			String mensaje = in.readUTF();
-			
-			System.out.println(mensaje);
-			
-			soc.close();
-			
-			
-			
-			
-			
-		} catch (IOException e) {
-			
-			e.printStackTrace();
-		}
-	}
+    String HOST = "localhost";
+    int PUERTO = 1234;
 
+    // declaration section:
+    // clientSocket: el socket de nuestro cliente 
+    // os: output stream
+    // is: input stream
+
+        Socket clienteSocket = null;  
+        DataOutputStream os = null;
+        BufferedReader is = null;
+
+    // Initialization section:
+    // tratar de abrir un socket en el purto dado
+   
+
+        try {
+        	clienteSocket = new Socket(HOST, PUERTO);
+            os = new DataOutputStream(clienteSocket.getOutputStream());
+            is = new BufferedReader(new InputStreamReader(clienteSocket.getInputStream()));
+        } catch (UnknownHostException e) {
+            System.err.println("host desconocido: " + HOST);
+        } catch (IOException e) {
+            System.err.println("No se pudo obtener el I/O para la coneccion a: " + HOST);
+        }
+
+    //si todo se ha inicializado entoces podremos ecribir algun dato
+    
+
+    if (clienteSocket == null || os == null || is == null) {
+        System.err.println( "Algo está mal. una variable es nula." );
+        return;
+    }
+
+    try {
+        while ( true ) {
+        	
+        System.out.print( "introduzca un mensage (escriba (parar) para parar la conección, y (salir) para salir del servidor");
+        System.out.println();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String keyboardInput = br.readLine();
+        os.writeBytes( keyboardInput + "\n" );
+
+        String n =( keyboardInput );
+        if ( n.equals("salir") || n.equals("parar") ) {
+            break;
+        }
+
+        String responseLine = is.readLine();
+        System.out.println("la respuesta del servidor es: " + responseLine);
+        }
+
+        // limpiar:
+        // cerar el output stream
+        // cerar el input stream
+        // cerar el socket
+
+        os.close();
+        is.close();
+        clienteSocket.close();   
+    } catch (UnknownHostException e) {
+        System.err.println("tratando de conectarse a un host deconocido: " + e);
+    } catch (IOException e) {
+        System.err.println("IOException:  " + e);
+    }
+    }           
 }
